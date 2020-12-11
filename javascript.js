26e4e3fb0cd2828589a70907cba006153ec24083
Vahe101 <74793307+Vahe101@users.noreply.github.com>
2020-12-11 20:49:21 +0400

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,35 +1,57 @@
+const cube = document.getElementById('cube');
 const windowHeight = window.innerHeight;
 const windowWidth = window.innerWidth;
 const stepLength = 50;
 
 var MarginLeft = 0;
 var MarginTop = 0;
-var cube = document.getElementById('cube');
+
+function move(element, options) {
+    element.style.marginLeft = options.marginLeft;
+    element.style.marginTop = options.marginTop;
+}
 
 function logKey(event) {
     if (Math.abs(MarginLeft) >= windowWidth || Math.abs(MarginTop) >= windowHeight) {
         MarginLeft = 0;
         MarginTop = 0;
-        cube.style.margin = 0;
+        move(cube, {
+            marginLeft: MarginLeft + "px",
+            marginTop: MarginTop + "px"
+        })
         return;
     }
+
     switch (event.code) {
         case 'ArrowLeft':
             MarginLeft -= stepLength;
-            cube.style.marginLeft = MarginLeft + "px";
+            move(cube, {
+                marginLeft: MarginLeft + "px",
+                marginTop: MarginTop + "px"
+            })
             break;
         case 'ArrowUp':
             MarginTop -= stepLength;
-            cube.style.marginTop = MarginTop + "px";
+            move(cube, {
+                marginLeft: MarginLeft + "px",
+                marginTop: MarginTop + "px"
+            })
             break;
         case 'ArrowRight':
             MarginLeft += stepLength;
-            cube.style.marginLeft = MarginLeft + "px";
+            move(cube, {
+                marginLeft: MarginLeft + "px",
+                marginTop: MarginTop + "px"
+            })
             break;
         case 'ArrowDown':
             MarginTop += stepLength;
-            cube.style.marginTop = MarginTop + "px";
+            move(cube, {
+                marginLeft: MarginLeft + "px",
+                marginTop: MarginTop + "px"
+            })
             break;
     }
 }
+
 document.addEventListener('keydown', logKey)
